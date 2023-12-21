4bc22b15031dc322e12017d34b44d6c024c45062
Andrés Leiva <aleiva1700@gmail.com>
2023-12-21 10:16:55 -0500

    feat(navigation): add basic structure of home and login page

---
+++ src/public/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import {ReactElement} from "react";
+
+export const HomePage = (): ReactElement => {
+  return (
+    <main>
+      <p>Home Page Component Works</p>
+    </main>
+  );
+};
---
+++ src/security/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import {ReactElement} from "react";
+
+export const LoginPage = (): ReactElement => {
+  return (
+    <main>
+      <p>Login Page Component Works</p>
+    </main>
+  );
+};
