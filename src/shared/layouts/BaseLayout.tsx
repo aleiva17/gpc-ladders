a65608c33fd30669873e2cae246966fd3eb11cb3
Andrés Leiva <aleiva1700@gmail.com>
2023-12-21 19:15:54 -0500

    feat(scaffolding): add base layout

---
+++ src/shared/layouts/BaseLayout.tsx
@@ -0,0 +1,17 @@
+import {ReactElement} from "react";
+import {AppToolbar} from "@/shared/components/AppToolbar.tsx";
+
+type BaseLayoutProps = {
+  children? : ReactElement | Array<ReactElement>
+}
+
+export const BaseLayout = ({ children }: BaseLayoutProps): ReactElement => {
+  return (
+    <div
+      className="grid grid-rows-[auto_1fr] bg-light dark:bg-dark dark:text-white min-h-screen"
+    >
+      <AppToolbar />
+      <main>{ children }</main>
+    </div>
+  );
+};
