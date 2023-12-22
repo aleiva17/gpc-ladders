cc90b27191a2d3c922b057f113a7dffd422646e6
Andrés Leiva <aleiva1700@gmail.com>
2023-12-21 22:09:02 -0500

    feat(themes): add theme switcher on toolbar

--- src/shared/components/AppToolbar.tsx
+++ src/shared/components/AppToolbar.tsx
@@ -1,12 +1,13 @@
 import {ReactElement} from "react";
 import {Link} from "react-router-dom";
 import {AvatarDropdown} from "@/shared/components/AvatarDropdown.tsx";
+import {RoundedThemeSwitcher} from "@/shared/components/RoundedThemeSwitcher.tsx";
 
 export const AppToolbar = (): ReactElement => {
-  const userIsLogged = true;
+  const userIsLogged = false;
 
   return (
-    <header className="sticky flex justify-between md:justify-around items-center top-0 p-4">
+    <header className="sticky flex justify-between md:justify-around items-center dark:bg-darkest top-0 p-4">
       <Link to="/" className="flex justify-center items-center hover:scale-95 duration-300 gap-2">
         <img
           src="/gpc-logo.png"
@@ -15,7 +16,11 @@ export const AppToolbar = (): ReactElement => {
         />
         <span className="font-extrabold text-xl md:text-4xl">GPC Ladders</span>
       </Link>
-      { userIsLogged && <AvatarDropdown /> }
+      {
+        userIsLogged
+          ? <AvatarDropdown />
+          : <RoundedThemeSwitcher />
+      }
     </header>
   );
 };
