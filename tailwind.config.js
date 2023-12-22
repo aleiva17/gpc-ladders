17f5d9d094be66cc50f96e961727d4c6c2283d80
Andrés Leiva <aleiva1700@gmail.com>
2023-12-21 19:14:42 -0500

    feat(scaffolding): add custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
+const colors = require('tailwindcss/colors');
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gpc: {
+          purple: "#8C52FF",
+          aqua: "#5CE1E6"
+        },
+        dark: "#24253e",
+        darkest: colors.slate["900"],
+        light: "#ffffff",
+        complementary: {
+          light: colors.slate["700"],
+          dark: colors.slate["300"]
+        }
+      }
+    },
   },
   plugins: [],
 }
