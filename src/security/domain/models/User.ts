dc90ba00fac2aaaa1d05154fe92d85ff174885e2
Andrés Leiva <aleiva1700@gmail.com>
2023-12-24 18:53:27 -0500

    feat(security): add User interface

---
+++ src/security/domain/models/User.ts
@@ -0,0 +1,16 @@
+
+export interface User {
+  avatar: string;
+  contribution: number;
+  country: string;
+  friendOfCount: number;
+  handle: string;
+  lastOnlineTimeSeconds: number;
+  maxRank: string;
+  maxRating: number;
+  organization: string;
+  rank: string;
+  rating: number;
+  registrationTimeSeconds: number;
+  titlePhoto: string;
+}
