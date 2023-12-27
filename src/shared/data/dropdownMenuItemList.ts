363fe09f75d71c62f3f9c5726d0b9102a12e7fa4
Andrés Leiva <aleiva1700@gmail.com>
2023-12-26 21:05:24 -0500

    feat(navigation): add array of groups of menu items

---
+++ src/shared/data/dropdownMenuItemList.ts
@@ -0,0 +1,15 @@
+import {JSXElementConstructor} from "react";
+import {MenuItemProps} from "@/shared/prefabs/navigation-menu-items/utils.ts";
+import {LogoutMenuItem} from "@/shared/prefabs/navigation-menu-items/LogoutMenuItem.tsx";
+import {ThemeSwitcherMenuItem} from "@/shared/prefabs/navigation-menu-items/ThemeSwitcherMenuItem.tsx";
+import {SettingsMenuItem} from "@/shared/prefabs/navigation-menu-items/SettingsMenuItem.tsx";
+import {ProblemListMenuItem} from "@/shared/prefabs/navigation-menu-items/ProblemListMenuItem.tsx";
+import {CopyTrainingMenuItem} from "@/shared/prefabs/navigation-menu-items/CopyTrainingMenuItem.tsx";
+import {AchievementsMenuItem} from "@/shared/prefabs/navigation-menu-items/AchievementsMenuItem.tsx";
+import {StatisticsMenuItem} from "@/shared/prefabs/navigation-menu-items/StatisticsMenuItem.tsx";
+
+export const dropdownMenuItemList: Array<Array<JSXElementConstructor<MenuItemProps>>> = [
+  [ProblemListMenuItem, CopyTrainingMenuItem, StatisticsMenuItem, AchievementsMenuItem],
+  [ThemeSwitcherMenuItem, SettingsMenuItem],
+  [LogoutMenuItem]
+];
