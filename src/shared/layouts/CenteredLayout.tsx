591f7063812e23d209a2afa59316563b5a78cb38
Andrés Leiva <aleiva1700@gmail.com>
2023-12-26 21:18:42 -0500

    feat(scaffolding): add centered layout

---
+++ src/shared/layouts/CenteredLayout.tsx
@@ -0,0 +1,19 @@
+import {ReactElement} from "react";
+import {AppToolbar} from "@/shared/components/AppToolbar.tsx";
+
+type CenteredLayoutProps = {
+  children? : ReactElement | Array<ReactElement>
+}
+
+export const CenteredLayout = ({ children }: CenteredLayoutProps): ReactElement => {
+  return (
+    <div
+      className="grid grid-rows-[auto_1fr] bg-light dark:bg-dark dark:text-white min-h-screen"
+    >
+      <AppToolbar />
+      <div className="flex justify-center items-center">
+        <main className="max-w-screen-2xl w-full p-4">{children}</main>
+      </div>
+    </div>
+  );
+};
