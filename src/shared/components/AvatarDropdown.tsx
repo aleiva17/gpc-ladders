4c1070b06136a378cfe989cdafb7ed749834143e
Andrés Leiva <aleiva1700@gmail.com>
2023-12-26 21:05:45 -0500

    feat(navigation): add menu items with animation

--- src/shared/components/AvatarDropdown.tsx
+++ src/shared/components/AvatarDropdown.tsx
@@ -1,7 +1,8 @@
-import {ReactElement} from "react";
-import { Menu } from '@headlessui/react';
-import {Link} from "react-router-dom";
+import {ReactElement, Fragment, JSXElementConstructor} from "react";
+import {Menu, Transition} from '@headlessui/react';
 import {User} from "@/security/domain/models/User.ts";
+import {dropdownMenuItemList} from "@/shared/data/dropdownMenuItemList.ts";
+import {MenuItemProps} from "@/shared/prefabs/navigation-menu-items/utils.ts";
 
 type AvatarDropdownProps = {
   user: User
@@ -21,21 +22,32 @@ export const AvatarDropdown = ({ user }: AvatarDropdownProps): ReactElement => {
           { user.handle }
         </span>
       </Menu.Button>
-      <Menu.Items className="origin-top-right absolute right-0 top-12 mt-2 w-56 rounded-md shadow-lg bg-white dark:bg-darkest ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
-        <div className="py-1">
-          <Menu.Item>
-            <Link
-              to="/"
-              className="flex items-center w-full px-4 py-2 text-complementary-light dark:text-complementary-dark hover:text-gpc-purple dark:hover:text-gpc-aqua gap-3"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
-              </svg>
-              Example
-            </Link>
-          </Menu.Item>
-        </div>
-      </Menu.Items>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items
+          className="origin-top-right absolute right-0 top-12 mt-2 w-56 rounded-md shadow-lg bg-white dark:bg-darkest ring-1 ring-black ring-opacity-5 divide-y divide-gray-200 dark:divide-gray-700 focus:outline-none">
+          {
+            dropdownMenuItemList.map((group: Array<JSXElementConstructor<MenuItemProps>>, index) =>
+              <div className="py-1" key={index}>
+                {
+                  group.map((ElementStrategy, index) =>
+                    <Menu.Item key={index}>
+                      {({ active }) => <ElementStrategy active={active} />}
+                    </Menu.Item>
+                  )
+                }
+              </div>
+            )
+          }
+        </Menu.Items>
+      </Transition>
     </Menu>
   );
 };
