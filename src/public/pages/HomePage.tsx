ade3d367d8808b53d9abe6a47dd85f791eb4dce8
Andrés Leiva <aleiva1700@gmail.com>
2023-12-30 16:47:12 -0500

    feat(home): add feature list on home page

--- src/public/pages/HomePage.tsx
+++ src/public/pages/HomePage.tsx
@@ -1,9 +1,18 @@
 import {ReactElement} from "react";
+import {CenteredLayout} from "@/shared/layouts/CenteredLayout.tsx";
+import {useUserStore} from "@/security/stores/useUserStore.ts";
+import {NormalizeHandlePipe} from "@/shared/pipes/NormalizeHandlePipe.ts";
+import {FeatureList} from "@/public/components/FeatureList.tsx";
 
 export const HomePage = (): ReactElement => {
+  const user = useUserStore(state => state.user!);
+  const displayableName = user.preferredHandle ?? NormalizeHandlePipe.transform(user.handle);
+
   return (
-    <main>
-      <p>Home Page Component Works</p>
-    </main>
+    <CenteredLayout>
+      <p className="text-2xl md:text-3xl lg:text-5xl font-bold">Welcome back, {displayableName}!</p>
+      <p className="mt-1 mb-4">Select one of the following options:</p>
+      <FeatureList />
+    </CenteredLayout>
   );
 };
