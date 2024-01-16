bc833d274ccf385c4f576c3dbde162fc06bcd6e4
Andrés Leiva <aleiva1700@gmail.com>
2024-01-16 07:40:02 -0500

    feat(problem-list): add problem list mock data

---
+++ src/problem-list/data/problem-lists.ts
@@ -0,0 +1,513 @@
+import {ProblemListDetail} from "@/problem-list/domain/model/ProblemListDetail.ts";
+
+type ProblemListContent = {
+  [key: string]: ProblemListDetail
+}
+
+export const problemLists: ProblemListContent = {
+  "a2oj-1300": {
+    title: "Classic A2OJ < 1300",
+    problems: [
+      {
+        title: "Young Physicist",
+        contestId: "69", index: "A",
+        rating: 1000, tags: ["implementation", "math"]
+      },
+      {
+        title: "Beautiful Matrix",
+        contestId: "263", index: "A",
+        rating: 800, tags: ["implementation"]
+      },
+      {
+        title: "Queue at the School",
+        contestId: "266", index: "B",
+        rating: 800, tags: ["constructive algorithms", "graph matchings", "implementation", "shortest paths"]
+      },
+      {
+        title: "Borze",
+        contestId: "32", index: "B",
+        rating: 800, tags: ["expression parsing", "implementation"]
+      },
+      {
+        title: "Beautiful Year",
+        contestId: "271", index: "A",
+        rating: 800, tags: ["brute force"]
+      },
+      {
+        title: "Lights Out",
+        contestId: "275", index: "A",
+        rating: 900, tags: ["implementation"]
+      },
+      {
+        title: "Word",
+        contestId: "59", index: "A",
+        rating: 800, tags: ["implementation", "strings"]
+      },
+      {
+        title: "Word Capitalization",
+        contestId: "281", index: "A",
+        rating: 800, tags: ["implementation", "strings"]
+      },
+      {
+        title: "Nearly Lucky Number",
+        contestId: "110", index: "A",
+        rating: 800, tags: ["implementation"]
+      },
+      {
+        title: "Stones on the Table",
+        contestId: "266", index: "A",
+        rating: 800, tags: ["implementation"]
+      },
+      {
+        title: "Panorami's Prediction",
+        contestId: "80", index: "A",
+        rating: 800, tags: ["brute force"]
+      },
+      {
+        title: "Ultra-Fast Mathematician",
+        contestId: "61", index: "A",
+        rating: 800, tags: ["implementation"]
+      },
+      {
+        title: "Perfect Permutation",
+        contestId: "233", index: "A",
+        rating: 800, tags: ["implementation", "math"]
+      },
+      {
+        title: "Arrival of the General",
+        contestId: "144", index: "A",
+        rating: 800, tags: ["implementation"]
+      },
+      {
+        title: "Drinks",
+        contestId: "200", index: "B",
+        rating: 800, tags: ["implementation", "math"]
+      },
+      {
+        title: "Insomnia cure",
+        contestId: "148", index: "A",
+        rating: 800, tags: ["constructive algorithms", "implementation", "math"]
+      },
+      {
+        title: "Cupboards",
+        contestId: "248", index: "A",
+        rating: 800, tags: ["implementation"]
+      },
+      {
+        title: "I_love_username",
+        contestId: "155", index: "A",
+        rating: 800, tags: ["brute force"]
+      },
+      {
+        title: "Tram",
+        contestId: "116", index: "A",
+        rating: 800, tags: ["implementation"]
+      },
+      {
+        title: "Helpful Maths",
+        contestId: "339", index: "A",
+        rating: 800, tags: ["greedy", "implementation", "sortings", "strings"]
+      },
+      {
+        title: "Is your horseshoe on the other hoof?",
+        contestId: "228", index: "A",
+        rating: 800, tags: ["implementation"]
+      },
+      {
+        title: "Way Too Long Words",
+        contestId: "71", index: "A",
+        rating: 800, tags: ["strings"]
+      },
+      {
+        title: "Boy or Girl",
+        contestId: "236", index: "A",
+        rating: 800, tags: ["brute force", "implementation", "strings"]
+      },
+      {
+        title: "Amusing Joke",
+        contestId: "141", index: "A",
+        rating: 800, tags: ["implementation", "sortings", "strings"]
+      },
+      {
+        title: "Soft Drinking",
+        contestId: "151", index: "A",
+        rating: 800, tags: ["implementation", "math"]
+      },
+      {
+        title: "HQ9+",
+        contestId: "133", index: "A",
+        rating: 900, tags: ["implementation"]
+      },
+      {
+        title: "Petya and Strings",
+        contestId: "112", index: "A",
+        rating: 800, tags: ["implementation", "strings"]
+      },
+      {
+        title: "Team",
+        contestId: "231", index: "A",
+        rating: 800, tags: ["brute force", "greedy"]
+      },
+      {
+        title: "Bit++",
+        contestId: "282", index: "A",
+        rating: 800, tags: ["implementation"]
+      },
+      {
+        title: "Effective Approach",
+        contestId: "227", index: "B",
+        rating: 1100, tags: ["implementation"]
+      },
+      {
+        title: "Dima and Friends",
+        contestId: "272", index: "A",
+        rating: 1000, tags: ["implementation", "math"]
+      },
+      {
+        title: "Jzzhu and Children",
+        contestId: "450", index: "A",
+        rating: 1000, tags: ["implementation"]
+      },
+      {
+        title: "Supercentral Point",
+        contestId: "165", index: "A",
+        rating: 1000, tags: ["implementation"]
+      },
+      {
+        title: "Petr and Book",
+        contestId: "139", index: "A",
+        rating: 1000, tags: ["implementation"]
+      },
+      {
+        title: "Parallelepiped",
+        contestId: "224", index: "A",
+        rating: 1100, tags: ["brute force", "geometry", "math"]
+      },
+      {
+        title: "Reconnaissance 2",
+        contestId: "34", index: "A",
+        rating: 800, tags: ["implementation"]
+      },
+      {
+        title: "Even Odds",
+        contestId: "318", index: "A",
+        rating: 900, tags: ["math"]
+      },
+      {
+        title: "Little Elephant and Rozdil",
+        contestId: "205", index: "A",
+        rating: 900, tags: ["brute force", "implementation"]
+      },
+      {
+        title: "Hexadecimal's theorem",
+        contestId: "199", index: "A",
+        rating: 900, tags: ["brute force", "constructive algorithms", "implementation", "number theory"]
+      },
+      {
+        title: "Jeff and Digits",
+        contestId: "352", index: "A",
+        rating: 1000, tags: ["brute force", "implementation", "math"]
+      },
+      {
+        title: "Xenia and Ringroad",
+        contestId: "339", index: "B",
+        rating: 1000, tags: ["implementation"]
+      },
+      {
+        title: "Magic Numbers",
+        contestId: "320", index: "A",
+        rating: 900, tags: ["brute force", "greedy"]
+      },
+      {
+        title: "Translation",
+        contestId: "41", index: "A",
+        rating: 800, tags: ["implementation", "strings"]
+      },
+      {
+        title: "Football",
+        contestId: "43", index: "A",
+        rating: 1000, tags: ["strings"]
+      },
+      {
+        title: "Bicycle Chain",
+        contestId: "215", index: "A",
+        rating: 900, tags: ["brute force", "implementation"]
+      },
+      {
+        title: "Sale",
+        contestId: "34", index: "B",
+        rating: 900, tags: ["greedy", "sortings"]
+      },
+      {
+        title: "System of Equations",
+        contestId: "214", index: "A",
+        rating: 800, tags: ["brute force"]
+      },
+      {
+        title: "Business trip",
+        contestId: "149", index: "A",
+        rating: 900, tags: ["greedy", "implementation", "sortings"]
+      },
+      {
+        title: "Dubstep",
+        contestId: "208", index: "A",
+        rating: 900, tags: ["strings"]
+      },
+      {
+        title: "k-String",
+        contestId: "219", index: "A",
+        rating: 1000, tags: ["implementation", "strings"]
+      },
+      {
+        title: "The number of positions",
+        contestId: "124", index: "A",
+        rating: 1000, tags: ["math"]
+      },
+      {
+        title: "Football",
+        contestId: "96", index: "A",
+        rating: 900, tags: ["implementation", "strings"]
+      },
+      {
+        title: "String Task",
+        contestId: "118", index: "A",
+        rating: 1000, tags: ["implementation", "strings"]
+      },
+      {
+        title: "Little Elephant and Function",
+        contestId: "221", index: "A",
+        rating: 1000, tags: ["implementation", "math"]
+      },
+      {
+        title: "Present from Lena",
+        contestId: "118", index: "B",
+        rating: 1000, tags: ["constructive algorithms", "implementation"]
+      },
+      {
+        title: "Dragons",
+        contestId: "230", index: "A",
+        rating: 1000, tags: ["greedy", "sortings"]
+      },
+      {
+        title: "Puzzles",
+        contestId: "337", index: "A",
+        rating: 900, tags: ["greedy"]
+      },
+      {
+        title: "Chat room",
+        contestId: "58", index: "A",
+        rating: 1000, tags: ["greedy", "strings"]
+      },
+      {
+        title: "Airport",
+        contestId: "218", index: "B",
+        rating: 1100, tags: ["implementation"]
+      },
+      {
+        title: "DZY Loves Chessboard",
+        contestId: "445", index: "A",
+        rating: 1200, tags: ["dfs and similar", "implementation"]
+      },
+      {
+        title: "Pashmak and Flowers",
+        contestId: "459", index: "B",
+        rating: 1300, tags: ["combinatorics", "implementation", "sortings"]
+      },
+      {
+        title: "Jeff and Periods",
+        contestId: "352", index: "B",
+        rating: 1300, tags: ["implementation", "sortings"]
+      },
+      {
+        title: "Little Girl and Game",
+        contestId: "276", index: "B",
+        rating: 1300, tags: ["games", "greedy"]
+      },
+      {
+        title: "Sail",
+        contestId: "298", index: "B",
+        rating: 1200, tags: ["brute force", "greedy", "implementation"]
+      },
+      {
+        title: "Shower Line",
+        contestId: "431", index: "B",
+        rating: 1200, tags: ["brute force", "implementation"]
+      },
+      {
+        title: "Shooshuns and Sequence ",
+        contestId: "222", index: "A",
+        rating: 1200, tags: ["brute force", "implementation"]
+      },
+      {
+        title: "Xenia and Divisors",
+        contestId: "342", index: "A",
+        rating: 1200, tags: ["greedy", "implementation"]
+      },
+      {
+        title: "Letter",
+        contestId: "43", index: "B",
+        rating: 1100, tags: ["implementation", "strings"]
+      },
+      {
+        title: "Kitahara Haruki's Gift",
+        contestId: "433", index: "A",
+        rating: 1100, tags: ["brute force", "implementation"]
+      },
+      {
+        title: "Comparing Strings",
+        contestId: "186", index: "A",
+        rating: 1100, tags: ["implementation", "strings"]
+      },
+      {
+        title: "Hungry Sequence",
+        contestId: "327", index: "B",
+        rating: 1200, tags: ["math"]
+      },
+      {
+        title: "Big Segment",
+        contestId: "242", index: "B",
+        rating: 1100, tags: ["implementation", "sortings"]
+      },
+      {
+        title: "Little Elephant and Bits",
+        contestId: "258", index: "A",
+        rating: 1100, tags: ["greedy", "math"]
+      },
+      {
+        title: "Yaroslav and Permutations",
+        contestId: "296", index: "A",
+        rating: 1100, tags: ["greedy", "math"]
+      },
+      {
+        title: "Fence",
+        contestId: "363", index: "B",
+        rating: 1100, tags: ["brute force", "dp"]
+      },
+      {
+        title: "TL",
+        contestId: "350", index: "A",
+        rating: 1200, tags: ["brute force", "greedy", "implementation"]
+      },
+      {
+        title: "Increase and Decrease",
+        contestId: "246", index: "B",
+        rating: 1300, tags: ["greedy", "math"]
+      },
+      {
+        title: "Two Bags of Potatoes",
+        contestId: "239", index: "A",
+        rating: 1200, tags: ["greedy", "implementation", "math"]
+      },
+      {
+        title: "Unlucky Ticket",
+        contestId: "160", index: "B",
+        rating: 1100, tags: ["greedy", "sortings"]
+      },
+      {
+        title: "Boys and Girls",
+        contestId: "253", index: "A",
+        rating: 1100, tags: ["greedy"]
+      },
+      {
+        title: "Easy Number Challenge",
+        contestId: "236", index: "B",
+        rating: 1300, tags: ["implementation", "number theory"]
+      },
+      {
+        title: "Pythagorean Theorem II",
+        contestId: "304", index: "A",
+        rating: 1200, tags: ["brute force", "math"]
+      },
+      {
+        title: "Cards with Numbers",
+        contestId: "254", index: "A",
+        rating: 1200, tags: ["constructive algorithms", "sortings"]
+      },
+      {
+        title: "Domino",
+        contestId: "353", index: "A",
+        rating: 1200, tags: ["implementation", "math"]
+      },
+      {
+        title: "Cinema Line",
+        contestId: "349", index: "A",
+        rating: 1100, tags: ["greedy", "implementation"]
+      },
+      {
+        title: "Rank List",
+        contestId: "166", index: "A",
+        rating: 1100, tags: ["binary search", "implementation", "sortings"]
+      },
+      {
+        title: "Cut Ribbon",
+        contestId: "189", index: "A",
+        rating: 1300, tags: ["brute force", "dp"]
+      },
+      {
+        title: "IQ Test",
+        contestId: "287", index: "A",
+        rating: 1100, tags: ["brute force", "implementation"]
+      },
+      {
+        title: "Building Permutation",
+        contestId: "285", index: "C",
+        rating: 1200, tags: ["greedy", "implementation", "sortings"]
+      },
+      {
+        title: "Kuriyama Mirai's Stones",
+        contestId: "433", index: "B",
+        rating: 1200, tags: ["dp", "implementation", "sortings"]
+      },
+      {
+        title: "T-primes",
+        contestId: "230", index: "B",
+        rating: 1300, tags: ["binary search", "implementation", "math", "number theory"]
+      },
+      {
+        title: "Sereja and Suffixes",
+        contestId: "368", index: "B",
+        rating: 1100, tags: ["data structures", "dp"]
+      },
+      {
+        title: "Flipping Game",
+        contestId: "327", index: "A",
+        rating: 1200, tags: ["brute force", "dp", "implementation"]
+      },
+      {
+        title: "Free Cash",
+        contestId: "237", index: "A",
+        rating: 1000, tags: ["implementation"]
+      },
+      {
+        title: "Polo the Penguin and Matrix",
+        contestId: "289", index: "B",
+        rating: 1400, tags: ["brute force", "dp", "implementation", "sortings", "ternary search"]
+      },
+      {
+        title: "Jzzhu and Sequences",
+        contestId: "450", index: "B",
+        rating: 1300, tags: ["implementation", "math"]
+      },
+      {
+        title: "Appleman and Card Game",
+        contestId: "462", index: "B",
+        rating: 1300, tags: ["greedy"]
+      },
+      {
+        title: "Sort the Array",
+        contestId: "451", index: "B",
+        rating: 1300, tags: ["implementation", "sortings"]
+      },
+      {
+        title: "Sereja and Bottles",
+        contestId: "315", index: "A",
+        rating: 1400, tags: ["brute force"]
+      },
+      {
+        title: "Adding Digits",
+        contestId: "260", index: "A",
+        rating: 1400, tags: ["implementation", "math"]
+      }
+    ]
+  }
+} as const;
