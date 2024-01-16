67230b11258d99e49841ca2c2c5f72c9701c5868
Andrés Leiva <aleiva1700@gmail.com>
2024-01-16 07:42:35 -0500

    feat(problem-list): add problem list table and page

---
+++ src/problem-list/components/ProblemListTable.tsx
@@ -0,0 +1,75 @@
+import {ReactElement, useState} from "react";
+import {Column} from "primereact/column";
+import {DataTable, DataTableFilterMeta} from "primereact/datatable";
+import {Problem} from "@/problem-list/domain/model/Problem.ts";
+import {ActionTemplate} from "@/problem-list/components/problemListTableTemplates/ActionTemplate.tsx";
+import {TagsTemplate} from "@/problem-list/components/problemListTableTemplates/TagsTemplate.tsx";
+import {TagRowFilterTemplate} from "@/problem-list/components/problemListTableTemplates/TagRowFilterTemplate.tsx";
+import {FilterMatchMode} from "primereact/api";
+import {getAllUniqueTagsFromProblems} from "@/problem-list/services/TagService.ts";
+
+type ProblemListTableProps = {
+  problems: Array<Problem>
+}
+// TODO: Fix multiple filtering on tags
+export const ProblemListTable = ({ problems }: ProblemListTableProps): ReactElement => {
+  const allTags = getAllUniqueTagsFromProblems(problems).sort();
+  const [filters] = useState<DataTableFilterMeta>({
+    title: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    tags: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    status: { value: null, matchMode: FilterMatchMode.EQUALS }
+  });
+
+  return (
+    <div className="grid">
+      <div className="overflow-x-auto">
+        <DataTable
+          value={problems}
+          stripedRows
+          tableStyle={{minWidth: "50rem"}}
+          removableSort
+          paginator
+          filters={filters}
+          rows={10}
+          rowsPerPageOptions={[5, 10, 25, 50, 100]}
+          filterDisplay="row"
+          emptyMessage="No problems found."
+        >
+          <Column
+            sortable
+            field="title"
+            header="Name"
+            filter
+            filterPlaceholder="Search by name"
+          />
+          <Column
+            sortable
+            field="rating"
+            header="Rating"
+          />
+          <Column
+            field="tags"
+            style={{ width: '20%' }}
+            header="Tags"
+            filter
+            showFilterOperator={true}
+            filterElement={(options) => TagRowFilterTemplate(options, allTags)}
+            filterField="tags"
+            showFilterMenu={false}
+            body={(problem: Problem) => TagsTemplate(problem.tags)}
+          />
+          <Column
+            field="category"
+            header="Status"
+          />
+          <Column
+            align="center"
+            className="w-1/12"
+            header="Actions"
+            body={ActionTemplate}
+          />
+        </DataTable>
+      </div>
+    </div>
+  );
+};
---
+++ src/problem-list/pages/ProblemListPage.tsx
@@ -0,0 +1,44 @@
+import {ReactElement, useEffect, useState} from "react";
+import {BaseLayout} from "@/shared/layouts/BaseLayout.tsx";
+import {Link, useNavigate, useParams} from "react-router-dom";
+import {problemGroupList} from "@/problem-list/data/problem-group-list.ts";
+import {problemLists} from "@/problem-list/data/problem-lists.ts";
+import {ProblemListDetail} from "@/problem-list/domain/model/ProblemListDetail.ts";
+import {ProblemListTable} from "@/problem-list/components/ProblemListTable.tsx";
+
+export const ProblemListPage = (): ReactElement => {
+  const { groupId, listId } = useParams();
+  const navigate = useNavigate();
+  const [problemList, setProblemList] = useState<ProblemListDetail | undefined>(undefined);
+
+  useEffect(() => {
+    const key = `${groupId}-${listId}`;
+
+    if (!Object.keys(problemGroupList).includes(groupId!) || !Object.keys(problemLists).includes(key)) {
+      navigate("/error-404");
+      return;
+    }
+    setProblemList(problemLists[key]);
+  }, [groupId, listId, navigate]);
+
+  return (
+    <BaseLayout>
+      <div className="flex justify-center">
+        <div className="flex flex-col w-full max-w-screen-xl p-6">
+          <h1
+            className="text-5xl font-bold border-b-2 border-complementary-light dark:border-complementary-dark w-fit py-4 self-center text-center">{problemList?.title}</h1>
+          <Link
+            to={`/problem-list/${groupId}`}
+            className="group flex items-center hover:text-gpc-purple dark:hover:text-gpc-aqua font-semibold gap-1 text-lg w-fit mt-12 mb-8"
+          >
+            <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+            </svg>
+            <span>Go back</span>
+          </Link>
+          <ProblemListTable problems={problemList?.problems ?? []} />
+        </div>
+      </div>
+    </BaseLayout>
+  );
+};
