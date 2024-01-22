a64c6dbec5f9b269859a520c087928266b81f09d
Andrés Leiva <aleiva1700@gmail.com>
2024-01-21 22:58:52 -0500

    feat(statistics): add programming language stats

---
+++ src/statistics/components/ProgrammingLanguagesStats.tsx
@@ -0,0 +1,27 @@
+import {Submission} from "@/problems/domain/model/Submission.ts";
+import {ReactElement} from "react";
+import {getProgrammingLanguagesSortedByFrequency} from "@/statistics/services/ProgrammingLanguagesService.ts";
+
+type ProgrammingLanguagesStatsProps = {
+  submissions: Array<Submission>;
+}
+
+export const ProgrammingLanguagesStats = ({submissions}: ProgrammingLanguagesStatsProps): ReactElement => {
+  const languagesStats = getProgrammingLanguagesSortedByFrequency(submissions);
+
+  return (
+    <div>
+      <h2 className="font-bold mb-2">Programming Languages</h2>
+      <div className="flex flex-col gap-2">
+        {
+          languagesStats.map((stat, id) => (
+            <div key={id} className="flex justify-between items-center">
+              <span className="font-medium bg-gray-200 rounded-full px-2 py-0.5">{stat.name}</span>
+              <span className="text-xs">{stat.frequency} problem{Math.abs(stat.frequency) !== 1 && 's'} solved</span>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+};
