c80a667f566ccf1bbfeba5cdc0d7a29b76d63e2a
Andrés Leiva <aleiva1700@gmail.com>
2024-01-21 22:58:25 -0500

    feat(statistics): add community stats

---
+++ src/statistics/components/CommunityStats.tsx
@@ -0,0 +1,31 @@
+import {User} from "@/security/domain/models/User.ts";
+import {ReactElement} from "react";
+import {getContributionColor} from "@/statistics/services/CodeforcesColorService.ts";
+
+type CommunityStatsProps = {
+  user: User;
+}
+
+export const CommunityStats = ({user}: CommunityStatsProps): ReactElement => {
+  const contributionSymbol = user.contribution > 0 ? "+" : "";
+
+  return (
+    <div>
+      <h2 className="font-bold mb-2">Community Stats</h2>
+      <div className="flex items-center gap-2 mb-1">
+        <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+             stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round"
+                d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"/>
+        </svg>
+        <span>Friends: <span className="font-semibold">{user.friendOfCount}</span> user{Math.abs(user.friendOfCount) !== 1 && 's'}</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"/>
+        </svg>
+        <span>Contribution: <span className={`font-semibold ${getContributionColor(user.contribution)}`}>{contributionSymbol}{user.contribution}</span> point{Math.abs(user.contribution) !== 1 && 's'}</span>
+      </div>
+    </div>
+  );
+};
