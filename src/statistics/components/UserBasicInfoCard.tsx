0adff7fa3633dea524a60621ec4161b56699637d
Andrés Leiva <aleiva1700@gmail.com>
2024-01-23 23:03:15 -0500

    fix(user-basic-info-card): add unique accepted submissions for correct displaying

--- src/statistics/components/UserBasicInfoCard.tsx
+++ src/statistics/components/UserBasicInfoCard.tsx
@@ -5,6 +5,7 @@ import {CommunityStats} from "@/statistics/components/CommunityStats.tsx";
 import {ProgrammingLanguagesStats} from "@/statistics/components/ProgrammingLanguagesStats.tsx";
 import {Submission} from "@/problems/domain/model/Submission.ts";
 import {TopSkillsStats} from "@/statistics/components/TopSkillsStats.tsx";
+import {getUniqueAcceptedSubmissions} from "@/problems/services/SubmissionService.ts";
 
 type UserBasicInfoCardProps = {
   user: User;
@@ -12,15 +13,17 @@ type UserBasicInfoCardProps = {
 }
 
 export const UserBasicInfoCard = ({user, submissions}: UserBasicInfoCardProps): ReactElement => {
+  const uniqueAcceptedSubmissions = getUniqueAcceptedSubmissions(submissions);
+
   return (
     <div className="bg-white dark:bg-darkest w-full h-fit rounded-xl drop-shadow p-4 md:p-6">
       <UserSummary user={user}/>
       <hr className="my-4 md:my-6"/>
       <CommunityStats user={user}/>
       <hr className="my-4 md:my-6"/>
-      <ProgrammingLanguagesStats submissions={submissions}/>
+      <ProgrammingLanguagesStats submissions={uniqueAcceptedSubmissions}/>
       <hr className="my-4 md:my-6"/>
-      <TopSkillsStats submissions={submissions} count={5}/>
+      <TopSkillsStats submissions={uniqueAcceptedSubmissions} count={5}/>
     </div>
   );
 };
