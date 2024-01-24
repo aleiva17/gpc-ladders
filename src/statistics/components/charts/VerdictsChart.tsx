91e3840d6c9c5bc0fd81d8d978466e5c68b4672d
Andrés Leiva <aleiva1700@gmail.com>
2024-01-23 21:53:27 -0500

    refactor(charts): replace props for inversion principle

--- src/statistics/components/charts/VerdictsChart.tsx
+++ src/statistics/components/charts/VerdictsChart.tsx
@@ -1,27 +1,26 @@
 import {ReactElement} from "react";
 import {Chart} from "primereact/chart";
-import {Submission} from "@/problems/domain/model/Submission.ts";
-import {getVerdictDataForVerdictRatioChart} from "@/statistics/services/ChartService.ts";
 import {TooltipItem} from "chart.js";
 
 type VerdictsChartProps = {
-  submissions: Array<Submission>;
+  data: object;
+  totalData: number;
 }
 
-export const VerdictsChart = ({submissions}: VerdictsChartProps): ReactElement => {
+export const VerdictsChart = ({data, totalData}: VerdictsChartProps): ReactElement => {
   return (
     <div className="bg-white dark:bg-darkest w-full rounded-xl drop-shadow p-4 md:p-6">
       <h2 className="font-bold mb-2">Ratio of verdicts</h2>
       <div className="flex justify-center min-w-0">
         <Chart
           type="pie"
-          data={getVerdictDataForVerdictRatioChart(submissions)}
+          data={data}
           options={{
             plugins: {
               tooltip: {
                 callbacks: {
                   label: function (context: TooltipItem<"pie">) {
-                    const percentage = (parseFloat(context.formattedValue) / submissions.length * 100).toFixed(2);
+                    const percentage = (parseFloat(context.formattedValue) / totalData * 100).toFixed(2);
                     return `${context.dataset.label}: ${context.formattedValue} (${percentage}%)`;
                   }
                 }
