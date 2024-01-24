019da09c7125863ef01051c6b60c3cf4315701a8
Andrés Leiva <aleiva1700@gmail.com>
2024-01-23 21:53:03 -0500

    refactor(charts): replace props for inversion principle

--- src/statistics/components/charts/ProblemRatingsChart.tsx
+++ src/statistics/components/charts/ProblemRatingsChart.tsx
@@ -1,20 +1,17 @@
 import {ReactElement} from "react";
-import {Submission} from "@/problems/domain/model/Submission.ts";
 import {Chart} from "primereact/chart";
-import {getRatingDataForProblemRatingsChart} from "@/statistics/services/ChartService.ts";
-import {getUniqueAcceptedSubmissions} from "@/problems/services/SubmissionService.ts";
 
 type ProblemRatingChartProps = {
-  submissions: Array<Submission>;
+  data: object;
 }
 
-export const ProblemRatingsChart = ({submissions}: ProblemRatingChartProps): ReactElement => {
+export const ProblemRatingsChart = ({data}: ProblemRatingChartProps): ReactElement => {
   return (
     <div className="bg-white dark:bg-darkest w-full rounded-xl drop-shadow p-4 md:p-6">
       <h2 className="font-bold mb-2">Solved problem ratings</h2>
       <Chart
         type="bar"
-        data={getRatingDataForProblemRatingsChart(getUniqueAcceptedSubmissions(submissions))}
+        data={data}
         options={{
           maintainAspectRatio: false,
           aspectRatio: 0.8,
