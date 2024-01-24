0e5ae4957058d1e7ade3d253b74b4944d89ddccf
Andrés Leiva <aleiva1700@gmail.com>
2024-01-23 21:53:48 -0500

    refactor(charts): replace old props with new ones

--- src/statistics/components/DetailedUserStats.tsx
+++ src/statistics/components/DetailedUserStats.tsx
@@ -3,17 +3,24 @@ import {Submission} from "@/problems/domain/model/Submission.ts";
 import {ProblemRatingsChart} from "@/statistics/components/charts/ProblemRatingsChart.tsx";
 import {VerdictsChart} from "@/statistics/components/charts/VerdictsChart.tsx";
 import {TagsChart} from "@/statistics/components/charts/TagsChart.tsx";
+import {
+  getRatingDataForProblemRatingsChart, getTagsDataForTagDoughnutChart,
+  getVerdictDataForVerdictRatioChart
+} from "@/statistics/services/ChartService.ts";
+import {getUniqueAcceptedSubmissions} from "@/problems/services/SubmissionService.ts";
 
 type DetailedUserStatsProps = {
   submissions: Array<Submission>;
 }
 
 export const DetailedUserStats = ({submissions}: DetailedUserStatsProps): ReactElement => {
+  const uniqueAcceptedSolutions = getUniqueAcceptedSubmissions(submissions);
+
   return (
     <div className="flex flex-col gap-4 lg:gap-6">
-      <ProblemRatingsChart submissions={submissions} />
-      <VerdictsChart submissions={submissions} />
-      <TagsChart submissions={submissions} />
+      <ProblemRatingsChart data={getRatingDataForProblemRatingsChart(uniqueAcceptedSolutions)} />
+      <VerdictsChart data={getVerdictDataForVerdictRatioChart(submissions)} totalData={submissions.length} />
+      <TagsChart data={getTagsDataForTagDoughnutChart(uniqueAcceptedSolutions)} />
     </div>
   );
 };
