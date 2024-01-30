7ae27edbbc566024876f357f2deeb06368ea7f47
Andrés Leiva <aleiva1700@gmail.com>
2024-01-29 22:58:24 -0500

    fix(theming): improve color on dark theme

--- src/settings/components/ChangeTheme.tsx
+++ src/settings/components/ChangeTheme.tsx
@@ -10,13 +10,13 @@ export const ChangeTheme = (): ReactElement => {
       <div className="flex font-medium text-complementary-light dark:text-complementary-dark gap-2">
         <button
           onClick={() => setTheme("light")}
-          className="hover:text-[#059bb4] hover:bg-gray-200 dark:hover:bg-gray-300 rounded-full px-4 py-1"
+          className="hover:text-gpc-purple-darker dark:hover:text-gpc-aqua-darker hover:bg-gray-200 dark:hover:bg-gray-700 rounded-full px-4 py-1"
         >
           Light
         </button>
         <button
           onClick={() => setTheme("dark")}
-          className="hover:text-[#059bb4] hover:bg-gray-200 dark:hover:bg-gray-300 rounded-full px-4 py-1"
+          className="hover:text-gpc-purple-darker dark:hover:text-gpc-aqua-darker hover:bg-gray-200 dark:hover:bg-gray-700 rounded-full px-4 py-1"
         >
           Dark
         </button>
