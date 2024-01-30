2b2e6ea70a33b2961f5b4216922526ba5d55164a
Andrés Leiva <aleiva1700@gmail.com>
2024-01-29 22:14:11 -0500

    fix(home): improve light and dark theme support

--- src/public/components/FeatureLinkCard.tsx
+++ src/public/components/FeatureLinkCard.tsx
@@ -14,12 +14,12 @@ export const FeatureLinkCard = ({ destination, logoUrl, logoAlt, title, descript
   return (
     <Link
       to={destination}
-      className={`flex items-center bg-gray-200 text-black rounded-xl ${ isSmall ? "h-32 md:h-28" : "h-56"} hover:scale-95 duration-300 gap-8 p-4`}
+      className={`flex items-center bg-white dark:bg-darkest drop-shadow-md text-black rounded-xl ${ isSmall ? "h-32 md:h-28" : "h-56"} hover:scale-95 duration-300 gap-8 p-4`}
     >
       <img src={logoUrl} alt={logoAlt} className="h-1/2 md:h-full w-auto" />
       <div>
-        <h2 className="text-2xl font-semibold">{title}</h2>
-        <p>{description}</p>
+        <h2 className="text-2xl font-semibold dark:text-white">{title}</h2>
+        <p className="dark:text-complementary-dark">{description}</p>
       </div>
     </Link>
   );
