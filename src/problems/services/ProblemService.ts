1b7a4b92c8728a618dc5a5689691d3df5cd6e74e
Andrés Leiva <aleiva1700@gmail.com>
2024-03-05 09:02:05 -0500

    fix(problem-service): update problem service endpoint

--- src/problems/services/ProblemService.ts
+++ src/problems/services/ProblemService.ts
@@ -5,7 +5,7 @@ import {ProblemListDetail} from "@/problems/domain/model/ProblemListDetail.ts";
 import {WebStorageCache} from "@/shared/models/WebStorageCache.ts";
 
 const instance = axios.create({
-  baseURL: "https://microhost.biz:8443/gpc/",
+  baseURL: "https://microhost.biz/gpc/",
   timeout: 10000
 });
 
